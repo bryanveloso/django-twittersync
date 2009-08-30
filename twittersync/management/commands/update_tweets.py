c681dfab5076b0d381590ef2e2023f34b4629024
Bryan Veloso <bryan@revyver.com>
2009-08-30 16:44:02 -0700

    Converting update_tweet's (management command) python-twitter calls to PTT's.

--- twittersync/management/commands/update_tweets.py
+++ twittersync/management/commands/update_tweets.py
@@ -1,34 +1,35 @@
 import twitter
-import dateutils
+import dateutil
+
 from dateutil.tz import tzlocal
 from django.core.management.base import BaseCommand
+from twitter import Twitter
 from twittersync.models import TwitterUser, Tweet
 
 class Command(BaseCommand):
-    help = "My shiny new management command."
+    help = 'Sync up with Twitter. For great justice.'
 
     def handle(self, *args, **options):
         for user in TwitterUser.objects.all():
-            api = twitter.Api(username=user.username, password=user.password)
-            timeline = api.GetUserTimeline()
+            api = Twitter(user.username, user.password)
+            timeline = api.statuses.user_timeline()
 
             # Update Twitter user data
-            userdata = api.GetUser(user.username)
+            userdata = api.users.show(screen_name=user.username)
 
-            user.twitter_id = userdata.id
-            user.location = userdata.location
-            user.name = userdata.name
-            user.image_url = userdata.profile_image_url
-            user.homepage = userdata.url
-            user.description = userdata.description
+            user.twitter_id = userdata['id']
+            user.location = userdata['location']
+            user.name = userdata['name']
+            user.image_url = userdata['profile_image_url']
+            user.homepage = userdata['url']
+            user.description = userdata['description']
             user.save()
 
             # Fetch tweets
             for tweet in timeline:
-                data = tweet.AsDict()
                 Tweet.objects.get_or_create(
                     twitter_user = user,
-                    tweet = data['text'],
-                    tweet_id = data['id'],
-                    posted = dateutils.parse(data['created_at']).astimezone(tzlocal()).replace(tzinfo=None)
+                    tweet = tweet['text'],
+                    tweet_id = tweet['id'],
+                    posted = dateutil.parser(tweet['created_at']).astimezone(tzlocal()).replace(tzinfo=None)
                 )
